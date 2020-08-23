5ce4a5b7ba494875cc691912664e682a07f0c549
Lukas <lukas.wiberg@hotmail.com>
2020-08-23 07:04:46 +0200

    Init
    Created a basic project with a scene, a basic tile texture, a grid script, some basic camera and mouse controls.

---
+++ Assets/Scripts/Control/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float speed = 0.1f;
+    void Update() {
+        Vector3 delta = Vector3.zero;
+        if(Input.GetKey(KeyCode.W)) {
+            delta += new Vector3(0, 0, -1);
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            delta += new Vector3(1, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.S)) {
+            delta += new Vector3(0, 0, 1);
+        }
+
+        if(Input.GetKey(KeyCode.D)) {
+            delta += new Vector3(-1, 0, 0);
+        }
+        if(delta != Vector3.zero) {
+            //TODO make the camera move the same dsitance when going diagonally
+            transform.position += delta.normalized * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Control/MouseController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    private GameObject selectedTile;
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, LayerMask.GetMask("Tile"))) {
+                selectedTile = hit.collider.gameObject;
+            }
+        }
+    }
+}
